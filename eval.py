8604d239a0633775d512ab856faad99a190ecc4b
capitanFlint129 <tekkengod@list.ru>
2024-02-02 17:04:42 +0300

    customize eval

--- eval.py
+++ eval.py
@@ -1,17 +1,29 @@
 import torch
-from absl import app
-from compiler_gym.envs import LlvmEnv
-from compiler_gym.leaderboard.llvm_instcount import eval_llvm_instcount_policy
+from compiler_gym.util.statistics import arithmetic_mean, geometric_mean
+from compiler_gym.util.timer import Timer
 
 from dqn import rollout, Agent
-
-
-def run(env: LlvmEnv) -> None:
-    agent = Agent(n_actions=15, input_dims=[69])
-    env.observation_space = "InstCountNorm"
-    agent.Q_eval.load_state_dict(torch.load("./H10-N4000-INSTCOUNTNORM.pth"))
-    rollout(agent, env)
-
+from train import make_env, config
 
 if __name__ == "__main__":
-    app.run(eval_llvm_instcount_policy(run))
+    env = make_env()
+    train_benchmarks = env.datasets[config["eval_benchmarks"]]
+
+    agent = Agent(input_dims=[125], n_actions=len(config["actions"]))
+    agent.Q_eval.load_state_dict(torch.load("models/mild-shadow-94.pth"))
+
+    rewards = []
+    times = []
+    for benchmark in train_benchmarks:
+        env.reset(benchmark=benchmark)
+        with Timer() as timer:
+            reward = rollout(agent, env)
+        print(f"{benchmark} - {reward} - {timer.time}")
+        rewards.append(reward)
+        times.append(timer.time)
+    print(f"Geomean reward: {geometric_mean(rewards)}")
+    print(f"Mean walltime: {arithmetic_mean(times)}")
+
+
+# Current mean walltime: 0.707s / benchmark.
+# Current geomean reward: 1.0146.
