46843a80143c302b492d8c18c8d1de72092a0d49
capitanFlint129 <tekkengod@list.ru>
2024-02-11 18:58:30 +0300

    simple changes

--- dqn/dqn.py
+++ dqn/dqn.py
@@ -21,7 +21,6 @@ import torch.optim as optim
 class DQN(nn.Module):
     def __init__(
         self,
-        alpha: float,
         observation_size: int,
         action_embedding_size: int,
         hidden_size: int,
@@ -45,24 +44,20 @@ class DQN(nn.Module):
             nn.Linear(fc_dims, fc_dims),
             nn.Linear(fc_dims, fc_dims),
         )
-        self.softmax = nn.Softmax(dim=-1)
-
-        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
-        self.loss = nn.SmoothL1Loss()  # try huber loss
 
     def forward(
         self,
         observation: torch.Tensor,
-        action: torch.LongTensor,
-        sequence_lengths: torch.LongTensor,
+        prev_action: torch.Tensor,
+        sequence_lengths: torch.Tensor,
     ) -> torch.LongTensor:
         assert (
-            observation.shape == action.shape and len(action.shape) == 3
-        ), f"{observation.shape} - {action.shape}"
-        action_embs = self.action_embedding(action)
+            observation.shape == prev_action.shape and len(prev_action.shape) == 3
+        ), f"{observation.shape} - {prev_action.shape}"
+        action_embs = self.action_embedding(prev_action)
         input = torch.cat((observation, action_embs), dim=-1)
         rnn_input = self.input_net(input)
-        output, (hn, cn) = self.rnn_encoder(rnn_input)
+        output, (_, _) = self.rnn_encoder(rnn_input)
         assert (
             len(output.shape) == 3
             and output.shape[0] == observation.shape[0]
@@ -70,8 +65,8 @@ class DQN(nn.Module):
             and output.shape[0] == observation.shape[2] == self.hidden_size
         ), f"{output.shape}"
         final_outputs = torch.index_select(output, 1, sequence_lengths - 1)
-        actions_probabilities = self.output_net(final_outputs)
-        return actions_probabilities
+        actions_q = self.output_net(final_outputs)
+        return actions_q
 
     def forward_step(
         self,
@@ -112,7 +107,6 @@ class Agent(nn.Module):
         # keep track of position of first available memory
         self.mem_cntr = 0
         self.Q_eval = DQN(
-            alpha=config["alpha"],
             observation_size=observation_size,
             action_embedding_size=config["action_embedding_size"],
             hidden_size=config["lstm_hidden_size"],
@@ -120,7 +114,6 @@ class Agent(nn.Module):
             n_actions=self.n_actions,
         )
         self.Q_next = DQN(
-            alpha=config["alpha"],
             observation_size=observation_size,
             action_embedding_size=config["action_embedding_size"],
             hidden_size=config["lstm_hidden_size"],
@@ -145,6 +138,10 @@ class Agent(nn.Module):
         self.prev_action = None
         self.to(self.device)
 
+        self.optimizer = optim.Adam(self.Q_eval.parameters(), lr=config["alpha"])
+        # todo : try huber loss
+        self.loss = nn.SmoothL1Loss()
+
     def episode_reset(self):
         self.h_prev = None
         self.c_prev = None
@@ -155,7 +152,6 @@ class Agent(nn.Module):
         self.actions_taken = []
 
     def store_transition(self, action, state, reward, new_state, done):
-        # what is the position of the first unoccupied memory
         index = self.mem_cntr % self.max_mem_size
         self.state_mem[index] = state
         self.new_state_mem[index] = new_state
@@ -166,46 +162,33 @@ class Agent(nn.Module):
 
     @torch.no_grad()
     def choose_action(self, observation, disable_epsilon_greedy=False):
-        # sends observation as tensor to device
-        # convert to float - > compiler gyms autophase vector is a long
         observation = observation.astype(np.float32)
         state = torch.tensor(observation, device=self.device)[None, ...]
-        actions_probabilities, hn, cn = self.Q_eval.forward_step(
+        actions_q, self.h_prev, self.c_prev = self.Q_eval.forward_step(
             state, self.prev_action
         )
         # network seems to choose same action over and over, even with zero reward,
         # trying giving negative reward for choosing same action multiple times
         # while torch.argmax(actions).item() in self.actions_taken:
-        #     # TODO здесь опять не работает
         #     actions[0][torch.argmax(actions).item()] = 0.0
-        action = torch.argmax(actions_probabilities).item()
-        # action = np.random.choice(
-        #     actions_probabilities.shape[0],
-        #     p=actions_probabilities.detach().cpu().numpy(),
-        # )
+        action = torch.argmax(actions_q).item()
         if not (np.random.random() > self.epsilon or disable_epsilon_greedy):
             action = np.random.choice(self.action_space)
         self.actions_taken.append(action)
-        self.h_prev = hn
-        self.c_prev = cn
         self.prev_action = action
         return action
 
     def replace_target_network(self):
         if self.learn_step_counter % self.replace_target_cnt == 0:
             self.Q_next.load_state_dict(self.Q_eval.state_dict())
+            self.Q_next.eval()
 
     def learn(self):
-        # start learning as soon as batch size of memory is filled
         if self.mem_cntr < self.learn_memory_threshold:
             return
-        # set gradients to zero
-        self.Q_eval.optimizer.zero_grad()
+        self.optimizer.zero_grad()
         self.replace_target_network()
-        # select subset of memorys
         max_mem = min(self.mem_cntr, self.max_mem_size)
-        # take a selection of the size of the batch size from the current pool of memory's
-        # pool of memory's will be full by the time we get here
         batch = np.random.choice(max_mem, self.batch_size, replace=False)
         # have to calculate scalar of importance so that we don't update network in a biased way
         batch_index = np.arange(self.batch_size, dtype=np.int32)
@@ -233,7 +216,7 @@ class Agent(nn.Module):
         loss = self.Q_eval.loss(q_target, q_eval).to(self.device)
         loss_val = loss.item()
         loss.backward()
-        self.Q_eval.optimizer.step()
+        self.optimizer.step()
         self.learn_step_counter += 1
 
         if self.epsilon > self.eps_end:
@@ -241,3 +224,17 @@ class Agent(nn.Module):
         else:
             self.epsilon = self.eps_end
         return loss_val
+
+
+def _get_range_for_cyclic(
+    begin_index: int, end_index: int, array_size: int
+) -> np.ndarray:
+    if end_index < begin_index:
+        end_index += array_size
+    indexes = np.arange(begin_index, end_index) % array_size
+    return indexes
+
+
+def _pad_seq_to_len(seq: np.ndarray, seq_len: int) -> np.ndarray:
+    pad_width = tuple([(0, seq_len - seq.shape[0])] + [(0, 0)] * (len(seq.shape) - 1))
+    return np.pad(seq, pad_width)
