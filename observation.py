d06ede6898f51decd50249109bed8758bea5e62a
capitanFlint129 <tekkengod@list.ru>
2024-02-21 09:24:23 +0300

    fix observation

--- observation.py
+++ observation.py
@@ -8,11 +8,26 @@ RAW_IR_OBSERVATION_NAME = "Ir"
 
 
 def get_observation(env, config) -> tuple[np.ndarray, bool]:
-    if config["observation_space"] == "AutophaseNorm":
-        observation = env.observation[config["observation_space"]]
+    observations = []
+    are_all_observations_correct = True
+    for observation_name in config["observation_space"]:
+        observation, is_observation_correct = _get_one_observation(
+            env, observation_name
+        )
+        are_all_observations_correct = (
+            are_all_observations_correct and is_observation_correct
+        )
+        observations.append(observation)
+    observation = np.concatenate(observations)
+    return observation, are_all_observations_correct
+
+
+def _get_one_observation(env, observation_name: str) -> tuple[np.ndarray, bool]:
+    if observation_name == "AutophaseNorm":
+        observation = env.observation["Autophase"]
         observation = observation / observation[51]
         return observation, _is_observation_correct(observation)
-    elif config["observation_space"] == "IR2Vec":
+    elif observation_name == "IR2Vec" or observation_name == "IR2VecNormalized":
         # observation = np.random.rand(300)
         result_queue = multiprocessing.Queue()
         ir_text = str(env.observation[RAW_IR_OBSERVATION_NAME])
@@ -22,10 +37,13 @@ def get_observation(env, config) -> tuple[np.ndarray, bool]:
         proc.start()
         proc.join()
         observation = result_queue.get()
+        if observation_name == "IR2VecNormalized":
+            # observation = normalize(observation, axis=0)
+            observation = observation / np.linalg.norm(observation)
         return observation, _is_observation_correct(observation)
-    else:
-        observation = env.observation[config["observation_space"]]
-        return observation, _is_observation_correct(observation)
+
+    observation = env.observation[observation_name]
+    return observation, _is_observation_correct(observation)
 
 
 def _get_ir2vec_observation(ir_text: str, result_queue: multiprocessing.Queue) -> None:
@@ -36,8 +54,6 @@ def _get_ir2vec_observation(ir_text: str, result_queue: multiprocessing.Queue) -
         observation = ir2vec.getProgramVector(init_obj)
     observation = np.array(observation)
     result_queue.put(observation)
-    # return np.array(observation)
-    # return np.random.rand(300)
 
 
 def _is_observation_correct(observation):
