036bd331ff11d51bccd97cf3634948d2a7e14b6d
capitanFlint129 <tekkengod@list.ru>
2024-03-16 18:24:48 +0300

    fix

--- config.py
+++ config.py
@@ -17,17 +17,13 @@ class TrainConfig:
     epsilon: float = 1.0  # The starting value for epsilon
     epsilon_end: float = 0.05  # The ending value for epsilon
     epsilon_dec: float = 5e-5  # The decrement value for epsilon
-    fc_dim: int = 512  # The dimension of a fully connected layer
+    fc_dim: int = 256  # The dimension of a fully connected layer
     lstm_hidden_size: int = 128  # The dimension of a fully connected layer
-    action_embedding_size: int = 100
     # Learning
     lr: float = 1e-4  # The learning rate
     tau: float = 0.99  # soft update coefficient
     batch_size: int = 512  # The batch size
     max_mem_size: int = 100000  # The maximum memory size
-    replace: int = (
-        500  # The number of iterations to run before replacing target network
-    )
     episodes: int = 4000  # The number of episodes used to learn
     validation_interval: int = 500  # The number of episodes used to learn
     episode_length: int = 50  # The (MAX) number of transformation passes per episode
