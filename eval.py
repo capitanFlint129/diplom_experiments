4215d6df81199886ff320202dd21f86908e5ca7e
capitanFlint129 <tekkengod@list.ru>
2024-03-18 23:37:33 +0300

    fixed

--- eval.py
+++ eval.py
@@ -1,8 +1,10 @@
+import os.path
+
 import torch
 
 from config import TrainConfig
 from dqn.train import validate
-from train import make_env, fix_seed, MODELS_DIR
+from train import make_env, fix_seed, MODELS_DIR, WANDB_PROJECT_NAME
 from utils import get_agent, prepare_datasets, get_last_model_wandb_naming
 
 if __name__ == "__main__":
@@ -18,11 +20,19 @@ if __name__ == "__main__":
         skipped=set(config.skipped_benchmarks),
     )
 
-    last_model_filename = get_last_model_wandb_naming(MODELS_DIR)
+    models_dir = os.path.join(MODELS_DIR, WANDB_PROJECT_NAME)
+    last_model_filename = get_last_model_wandb_naming(models_dir)
     print(f"Load model from {last_model_filename}\n")
-    agent = get_agent(config, device, f"{MODELS_DIR}/{last_model_filename}")
+    agent = get_agent(config, device, os.path.join(models_dir, last_model_filename))
     with torch.no_grad():
-        test_result = validate(agent, env, config, test_benchmarks, enable_logs=True)
+        test_result = validate(
+            agent,
+            env,
+            config,
+            test_benchmarks,
+            enable_logs=True,
+            use_actions_masking=True,
+        )
     env.close()
     print(f"Geomean reward: {test_result.geomean_reward}")
     print(f"Mean walltime: {test_result.mean_walltime}")
