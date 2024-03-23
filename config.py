c35c81658f80342b784bbeb75fb16b633a90c105
capitanFlint129 <tekkengod@list.ru>
2024-03-23 10:19:51 +0300

    refactor

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass, field
 
 from action_config import *
 
-WANDB_PROJECT_NAME = "rl-compilers-experiments-DQN-fix-results-2"
+WANDB_PROJECT_NAME = "trash"
 COMPILER_GYM_PATH = "~/.local/share/compiler_gym"
 LLVM_BINS_PATH = os.path.join(COMPILER_GYM_PATH, "llvm-v0/bin")
 MODELS_DIR = "_models"
