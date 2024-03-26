d8bcbbbd1fbac881b1484fdc2ecb958282c4ede2
flint <tekkengod@list.ru>
2024-03-26 10:31:07 +0300

    update cg eval

--- compiler_gym_leaderboard_eval.py
+++ compiler_gym_leaderboard_eval.py
@@ -1,20 +1,28 @@
+import os
+
 import torch
 from absl import app
 from compiler_gym.envs import LlvmEnv
 from compiler_gym.leaderboard.llvm_instcount import eval_llvm_instcount_policy
 
-from config import TrainConfig
+from config import MODELS_DIR, WANDB_PROJECT_NAME, TrainConfig
 from dqn.train import rollout
-from utils import fix_seed, get_agent
+from utils import fix_seed, get_agent, get_last_model_wandb_naming
 
 
 def run(env: LlvmEnv) -> None:
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    agent = get_agent(config, device, policy_net_path="_models/wandering-bee-129.pth")
-    rollout(agent, env, config)
+    rollout(agent, env, config, use_actions_masking=config.eval_with_forbidden_actions)
 
 
 if __name__ == "__main__":
+    config = TrainConfig()
+    use_actions_masking = True
+    models_dir = os.path.join(MODELS_DIR, WANDB_PROJECT_NAME)
+    last_model_filename = get_last_model_wandb_naming(models_dir)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Load model from {last_model_filename}\n")
+    
+    agent = get_agent(config, device, os.path.join(models_dir, last_model_filename))
     config = TrainConfig()
     fix_seed(config.random_state)
     app.run(eval_llvm_instcount_policy(run))
