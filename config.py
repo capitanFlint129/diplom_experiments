417fa23c490378173083ae83dc7b7b90d04297d6
capitanFlint129 <tekkengod@list.ru>
2024-03-27 10:39:54 +0300

    prev-3

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class TrainConfig:
     epsilon: float = 1.0  # The starting value for epsilon
     epsilon_end: float = 0.05  # The ending value for epsilon
     epsilon_dec: float = 5e-5  # The decrement value for epsilon
-    fc_dim: int = 128  # The dimension of a fully connected layer
+    fc_dim: int = 512  # The dimension of a fully connected layer
     lstm_hidden_size: int = 256  # The dimension of a fully connected layer
     # Learning
     lr: float = 1e-4  # The learning rate
@@ -27,8 +27,8 @@ class TrainConfig:
     max_mem_size: int = 100000  # The maximum memory size
     episodes: int = 4000  # The number of episodes used to learn
     validation_interval: int = 500  # The number of episodes used to learn
-    episode_length: int = 10  # The (MAX) number of transformation passes per episode
-    patience: int = 4  # The (MAX) number of times to apply a series of transformations without observable change
+    episode_length: int = 40  # The (MAX) number of transformation passes per episode
+    patience: int = 10  # The (MAX) number of times to apply a series of transformations without observable change
     val_patience: int = 5
     eval_with_forbidden_actions: bool = True
     learn_memory_threshold: int = max(batch_size, 32)
@@ -60,10 +60,10 @@ class TrainConfig:
             # "start-IR2Vec",
             # "remains-counter",
             "remains-counter-normalized",
-            # "prev-2",
+            "prev-3",
         ]
     )
-    observation_size: int = 70
+    observation_size: int = 208
     reward_space: str = "IrInstructionCountOz"
     actions: list = field(
         default_factory=lambda: COMPILER_GYM_LEADERBOARD_DQN_ACTION_SET
