3160f2ce618dc222da2b8855126857dfb3a13225
capitanFlint129 <tekkengod@list.ru>
2024-04-01 10:26:31 +0300

    performance

---
+++ env/performance_optimization.py
@@ -0,0 +1,8 @@
+from compiler_gym.wrappers import CompilerEnvWrapper
+
+# noinspection PyUnresolvedReferences
+from compiler_gym.wrappers import RuntimePointEstimateReward
+
+
+class LlvmMcaEnv(CompilerEnvWrapper):
+    pass
