7afb2be7f4c7b90d6be5260dc6d57dae15a2ce6a
flint <tekkengod@list.ru>
2024-04-04 11:17:03 +0300

    poset rl odg

--- config/config.py
+++ config/config.py
@@ -67,7 +67,7 @@ class TrainConfig:
     observation_size: int = 70
     reward_space: str = "IrInstructionCountOz"
     actions: list = field(
-        default_factory=lambda: POSET_RL_MANUAL
+        default_factory=lambda: POSET_RL_ODG
     )
     special_actions: list = field(
         default_factory=lambda: [
