836faa2d383fab1bdac9a56628edbcc33529c8e4
capitanFlint129 <tekkengod@list.ru>
2024-04-21 18:24:20 +0300

    format

--- env/performance_optimization.py
+++ env/performance_optimization.py
@@ -1,10 +1,8 @@
 import os
 import subprocess
 from pathlib import Path
-import tempfile
 
 from compiler_gym import CompilerEnv
-from compiler_gym.wrappers import CompilerEnvWrapper
 from compiler_gym.envs import llvm
 
 # noinspection PyUnresolvedReferences
@@ -12,27 +10,26 @@ from compiler_gym.wrappers import RuntimePointEstimateReward
 
 from config.config import TrainConfig
 
-
-LLVM_BINS = '/home/flint/.local/share/compiler_gym/llvm-v0/bin'
-MCA_BIN = os.path.join(LLVM_BINS, 'llvm-mca')
+LLVM_BINS = "/home/flint/.local/share/compiler_gym/llvm-v0/bin"
+MCA_BIN = os.path.join(LLVM_BINS, "llvm-mca")
 # CLANG_BIN = os.path.join(LLVM_BINS, 'clang')
-CLANG_BIN = 'clang'
-OPT_BIN = os.path.join(LLVM_BINS, 'opt')
-LLC_BIN = os.path.join(LLVM_BINS, 'llc')
+CLANG_BIN = "clang"
+OPT_BIN = os.path.join(LLVM_BINS, "opt")
+LLC_BIN = os.path.join(LLVM_BINS, "llc")
 JOTAI_BENCHMARKS_DIRS = [
     "/home/flint/diplom/jotai-benchmarks/benchmarks/anghaLeaves/",
     "/home/flint/diplom/jotai-benchmarks/benchmarks/anghaMath/",
 ]
-TMP_DIR = 'tmp'
+TMP_DIR = "tmp"
 
-O0 = '-O0'
-O1 = '-O1'
-O2 = '-O2'
-O3 = '-O3'
-OZ = '-Oz'
+O0 = "-O0"
+O1 = "-O1"
+O2 = "-O2"
+O3 = "-O3"
+OZ = "-Oz"
 
 
-class LlvmMcaEnv(CompilerEnv):
+class LlvmMcaEnv:
     def __init__(self, config: TrainConfig, env: CompilerEnv, observation_name: str):
         self._cg_env = env
         self._observation_name = observation_name
@@ -42,14 +39,17 @@ class LlvmMcaEnv(CompilerEnv):
         self._filepath = os.path.join(self._tmp_dir, self._filename)
         self._config = config
 
-
     def reset(self):
         attempts = 100
         for i in range(attempts):
             self._cg_env.reset()
             proc = subprocess.run(
                 [
-                    CLANG_BIN, O0, "--target=x86_64", "-emit-llvm", "-o", 
+                    CLANG_BIN,
+                    O0,
+                    "--target=x86_64",
+                    "-emit-llvm",
+                    "-o",
                     self._filepath,
                 ],
                 input=self._cg_env.benchmark.source,
@@ -61,7 +61,7 @@ class LlvmMcaEnv(CompilerEnv):
                 self._rblock_throughput_initial = get_rblock_throughput(self._filepath)
                 return
         raise Exception(f"Failed to reset after {attempts} attempts")
-    
+
     def step(self, action):
         rblock_throughput_before = get_rblock_throughput(self._filepath)
         flag = self._config.actions[action]
@@ -78,8 +78,10 @@ class LlvmMcaEnv(CompilerEnv):
             print(proc.stderr)
             raise Exception(f"Opt step failed {self._cg_env.benchmark}")
         rblock_throughput_after = get_rblock_throughput(self._filepath)
-        return (rblock_throughput_before - rblock_throughput_after) / self._rblock_throughput_initial
-        
+        return (
+            rblock_throughput_before - rblock_throughput_after
+        ) / self._rblock_throughput_initial
+
     def multistep(self):
         pass
 
@@ -87,7 +89,7 @@ class LlvmMcaEnv(CompilerEnv):
         space = self._cg_env.observation.spaces[obs_name]
         bitcode = Path(self._filepath)
         return llvm.compute_observation(space, bitcode)
-    
+
 
 def get_mca_result_from_ir(bc_path):
     proc = subprocess.run(
@@ -101,7 +103,7 @@ def get_mca_result_from_ir(bc_path):
 
 
 def parse_rblock_throughput(mca_output):
-    line = mca_output.split('\n', maxsplit=9)[8]
+    line = mca_output.split("\n", maxsplit=9)[8]
     # print(line)
     return float(line.split()[2])
 
