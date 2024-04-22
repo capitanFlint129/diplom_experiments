2db2d6011be7921533626fd0e34c1b0c68b8dcd6
flint <tekkengod@list.ru>
2024-04-22 15:04:39 +0300

    update

--- other_experiments/performance/o3_cbench_test.py
+++ other_experiments/performance/o3_cbench_test.py
@@ -5,8 +5,9 @@ from compiler_gym import CompilerEnv
 from compiler_gym.errors import BenchmarkInitError
 from tqdm import tqdm
 from compiler_gym.wrappers import RuntimePointEstimateReward
+import compiler_gym
 
-from config.action_config import O3_SEQ
+from config.action_config import O3_SEQ, O2_SEQ, O1_SEQ
 
 
 def apply_pass_sequence(env: CompilerEnv, pass_sequence):
@@ -19,6 +20,14 @@ def apply_pass_sequence(env: CompilerEnv, pass_sequence):
             # print(reward)
 
 
+# def get_runtime(env: CompilerEnv, n=10):
+#     runtimes = []
+#     for i in range(n):
+#         runtimes.append(env.observation["Runtime"][0])
+#     print(np.mean(runtimes), np.std(runtimes))
+#     return np.mean(runtimes)
+
+
 def main():
     env: CompilerEnv = gym.make("llvm-v0")
     # env = RuntimePointEstimateReward(
@@ -29,26 +38,51 @@ def main():
         "benchmark": [],
         "base_runtime": [],
         "o3_runtime": [],
+        "o3_speedup": [],
     }
-
+    
+    runtime_count = 30
     for benchmark in tqdm(benchmarks):
-        try:
-            env.reset(benchmark=benchmark)
-        except BenchmarkInitError:
-            print(f"Benchmark {benchmark} not runnable, skip it")
-            continue
-        if not env.observation["IsRunnable"]:
-            print(f"Benchmark {benchmark} not runnable, skip it")
-            continue
-        results["benchmark"].append(str(benchmark))
-        results["base_runtime"].append(env.observation["Runtime"])
-        apply_pass_sequence(env, O3_SEQ)
-        results["o3_runtime"].append(env.observation["Runtime"])
+        with compiler_gym.make("llvm-v0", benchmark=benchmark) as new_env:
+        # try:
+            # env.reset(benchmark=benchmark)
+        # except BenchmarkInitError:
+            # print(f"Benchmark {benchmark} not runnable, skip it")
+            # continue
+        # results["benchmark"].append(str(benchmark))
+        # results["base_runtime"].append(get_runtime(env))
+            new_env.reset()
+
+            if not new_env.observation["IsRunnable"]:
+                print(f"Benchmark {benchmark} not runnable, skip it")
+                continue
+
+            results["benchmark"].append(str(benchmark))
+
+
+            new_env.runtime_observation_count = runtime_count
+            new_env.runtime_warmup_count = 0
+            # new_env.apply(env.state)
+            final_runtimes = new_env.observation.Runtime()
+            assert len(final_runtimes) == runtime_count
+
+            new_env.reset()
+            # apply_pass_sequence(new_env, O1_SEQ)
+            # new_env.send_param("llvm.apply_baseline_optimizations", "-O1")
+            o3_runtimes = new_env.observation.Runtime()
+            assert len(o3_runtimes) == runtime_count
+            
+            speedup = np.median(o3_runtimes) / max(np.median(final_runtimes), 1e-12)
+
+            results["base_runtime"].append(np.median(final_runtimes))
+            results["o3_runtime"].append(np.median(o3_runtimes))
+            results["o3_speedup"].append(speedup)
 
     pd_results = pd.DataFrame(data=results)
     pd_results.to_csv("o3_cbench_test_results.csv")
     print(f'base_runtime mean: {np.mean(results["base_runtime"])}')
     print(f'o3_runtime mean: {np.mean(results["o3_runtime"])}')
+    print(f'o3_speedup mean: {np.mean(results["o3_speedup"])}')
 
 
 if __name__ == "__main__":
