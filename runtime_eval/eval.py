e4eefcbc653d3c857521b0c2112cd905a7e2ea1b
capitanFlint129 <tekkengod@list.ru>
2024-04-24 19:04:04 +0300

    update

--- runtime_eval/eval.py
+++ runtime_eval/eval.py
@@ -1,29 +1,57 @@
+import json
 import os
-import numpy as np
 import subprocess
 
 import pandas as pd
+from tqdm import tqdm
 
 DATASET_URI = "benchmark://jotaibench-v0"
 RUN_NAME = "lively-snowflake-49"
-
-
 RUNS = 30
 
 
 def parse_exec_time(time_stdout) -> float:
-    # real	0m0,001s
-    pass
+    time_str = time_stdout.split("\n", maxsplit=1)[0]
+    minutes, time_str = time_str.split("m")
+    seconds = time_str.split("s")[0]
+    minutes = float(minutes.replace(",", "."))
+    seconds = float(seconds.replace(",", "."))
+    return minutes * 60 + seconds
 
 
-def get_runtimes(bin_path, runs=RUNS) -> list[float]:
-    runtimes = []
-    for i in range(runs):
-        proc = subprocess.run(["time", bin_path], capture_output=True)
-        runtimes.append(parse_exec_time(proc.stdout.decode()))
-        if proc.returncode != 0:
-            raise Exception("run failed")
-    return runtimes
+def parse_exec_time_hyperfine(time_stdout) -> float:
+    time_str = time_stdout.split("\n", maxsplit=1)[0]
+    minutes, time_str = time_str.split("m")
+    seconds = time_str.split("s")[0]
+    minutes = float(minutes.replace(",", "."))
+    seconds = float(seconds.replace(",", "."))
+    return minutes * 60 + seconds
+
+
+def measure_execution_mean_and_std(
+    bin_path, execution_args: str = "", runs=RUNS
+) -> tuple[float, float]:
+    # with Timer() as timer:
+    #     proc = subprocess.run(
+    #         [bin_path] + execution_args.split(),
+    #         capture_output=True,
+    #         # shell=True,
+    #     )
+    # if proc.returncode != 0:
+    #     print(proc.stderr)
+    #     raise Exception(f"run failed: {proc.stderr}")
+    # runtimes.append(timer.time)
+    proc = subprocess.run(
+        f"hyperfine '{bin_path} {execution_args}' --export-json hyperfine_result.json --show-output",
+        shell=True,
+        capture_output=True,
+    )
+    if proc.returncode != 0:
+        print(proc.stderr)
+        raise Exception(f"run failed: {proc.stderr}")
+    with open("hyperfine_result.json", "r") as hyperfine_result:
+        result = json.load(hyperfine_result)
+    return result["results"][0]["mean"], result["results"][0]["stddev"]
 
 
 def main():
@@ -38,19 +66,30 @@ def main():
         "O3 speedup": [],
         "O0 speedup": [],
     }
+    execution_args = "0"
     benchmarks = sorted(os.listdir("data/model"))
-    for benchmark in benchmarks:
-        o3_runtimes = get_runtimes(f"time data/O3/{benchmark}")
-        o3_runtimes_mean = np.mean(o3_runtimes)
-        o3_runtimes_std = np.std(o3_runtimes)
+    for benchmark in tqdm(benchmarks):
+        o3_runtimes_mean, o3_runtimes_std = measure_execution_mean_and_std(
+            f"data/O3/{benchmark}", execution_args=execution_args
+        )
 
-        o0_runtimes = get_runtimes(f"time data/O0/{benchmark}")
-        o0_runtimes_mean = np.mean(o0_runtimes)
-        o0_runtimes_std = np.std(o0_runtimes)
+        o0_runtimes_mean, o0_runtimes_std = measure_execution_mean_and_std(
+            f"data/O0/{benchmark}", execution_args=execution_args
+        )
 
-        model_runtimes = get_runtimes(f"time data/model/{benchmark}")
-        model_runtimes_mean = np.mean(model_runtimes)
-        model_runtimes_std = np.std(model_runtimes)
+        model_runtimes_mean, model_runtimes_std = measure_execution_mean_and_std(
+            f"data/model/{benchmark}", execution_args=execution_args
+        )
+
+        result["benchmark"].append(str(benchmark).split("/")[-1])
+        result["O3 runtime, mean"].append(o3_runtimes_mean)
+        result["O3 runtime, std"].append(o3_runtimes_std)
+        result["O0 runtime, mean"].append(o0_runtimes_mean)
+        result["O0 runtime, std"].append(o0_runtimes_std)
+        result["model runtime, mean"].append(model_runtimes_mean)
+        result["model runtime, std"].append(model_runtimes_std)
+        result["O3 speedup"].append(o3_runtimes_mean / model_runtimes_mean)
+        result["O0 speedup"].append(o0_runtimes_mean / model_runtimes_mean)
 
     result_df = pd.DataFrame(data=result)
     print(result_df.drop(columns=["benchmark"]).mean())
